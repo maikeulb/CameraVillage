66c162fc9516eeb8cf02d842cfcb9bf503636018
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-08 19:17:03 -0500

    Add customerorder specification

---
+++ src/Specifications/CustomerOrdersWithItemsSpecification.cs
@@ -0,0 +1,14 @@
+using RolleiShop.Models.Entities.Order;
+
+namespace RolleiShop.Specifications
+{
+    public class CustomerOrdersWithItemsSpecification : BaseSpecification<Order>
+    {
+        public CustomerOrdersWithItemsSpecification(string buyerId)
+            : base(o => o.BuyerId == buyerId)
+        {
+            AddInclude(o => o.OrderItems);
+            AddInclude("OrderItems.ItemOrdered");
+        }
+    }
+}
